6a602a26ba41466427020ce8e1c41d3414f25b72
Adam Zawierucha <zawiedev@gmail.com>
2020-03-15 20:46:30 -0700

    initial xml reader

--- read_greetings.py
+++ read_greetings.py
@@ -1,7 +1,27 @@
-def exclaim(greeting,subject):
-    """
-    Prints desired output {greeting}, {subject}!
-    """
-    print("{}, {}!".format(greeting,subject))
+#Adam Zawierucha
+#OpenStax Internship Assessment
+#March 15th 2020
+from xml.dom import minidom
 
-exclaim("hello","world")
+def exclaim(greeting):
+    """
+    Input: greeting (tuple): from xml
+    Output: None
+    Prints desired output
+    """
+    prefix = greeting[0]
+    target = greeting[1]
+    print("{}, {}!".format(prefix,target))
+
+def getGreetings(file_name):
+    """
+    Input: filname_name (string): name of the final
+    Output: a list of tuples, where each tuple is a greeting (prefix,target)
+    """
+    xmldoc = minidom.parse(file_name)
+    greetings = list()
+    for greeting_tag in xmldoc.getElementsByTagName("greeting"):
+        print(greeting_tag)
+
+exclaim(("hello","world"))
+getGreetings("greetings.xml")
